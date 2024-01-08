14ed1e6af5601d1f12f5c51454fbe8f52eac5890
Muhammad Naqdi <naqdimuhammad@gmail.com>
2024-01-08 17:15:23 +0330

    Update setup-root.sh

--- setup-root.sh
+++ setup-root.sh
@@ -4,7 +4,7 @@ apt -y install sudo network-manager pcmanfm xorg xinit numlockx tmux xterm pulse
 apt -y install build-essential gdb linux-headers-amd64 cmake clang clangd libclang-dev default-jdk mit-scheme sbcl
 apt -y install emacs vim alacritty firefox-esr imagemagick vlc qbittorrent openconnect
 apt -y install git scrot fbcat unzip p7zip-full wget curl aria2 ffmpeg htop pkg-config rlwrap redshift texinfo pandoc blueman flatpak
-apt -y install xmonad libghc-xmonad-contrib-dev xmobar lxdm suckless-tools neofetch picom nitrogen ttf-bitstream-vera
+apt -y install xmonad libghc-xmonad-contrib-dev xmobar suckless-tools neofetch picom nitrogen ttf-bitstream-vera
 apt -y autopurge
 apt -y clean
 
